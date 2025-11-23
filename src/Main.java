a4531b88e59b8fd3e7a3c7201f911f1630b8a6a9
Chinh <chinhcongtu20@gmail.com>
2025-11-23 22:06:00 +0700

    Tich hop quan ly chung chi vao he thong

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
         CourseService courseService = new CourseService();
         ClassroomService classService = new ClassroomService(); // Them dong nay
         ScheduleService scheduleService = new ScheduleService();
-
+        CertificateService certService = new CertificateService();
         while (true) {
             System.out.println("\n=== QUAN LY TRUNG TAM ===");
             System.out.println("1. Them khoa Tieng Anh");
@@ -16,6 +16,8 @@ public class Main {
             System.out.println("5. Xem danh sach lop"); // Moi
             System.out.println("6. Xep lich hoc cho lop");
             System.out.println("7. Xem thoi khoa bieu");
+            System.out.println("10. Cap chung chi (Demo)");
+            System.out.println("11. Xem danh sach chung chi");
             System.out.println("0. Thoat");
             System.out.print("Chon: ");
 
@@ -28,6 +30,15 @@ public class Main {
                 case "5": classService.showAllClasses(); break;
                 case "6": scheduleService.addSchedule(); break;
                 case "7": scheduleService.showSchedules(); break;
+                case "10":
+                    // Demo cap bang nhanh
+                    System.out.print("Nhap ID HV: ");
+                    String id = scanner.nextLine();
+                    System.out.print("Nhap diem tong ket: ");
+                    double score = Double.parseDouble(scanner.nextLine());
+                    certService.issueCertificate(id, score);
+                    break;
+                case "11": certService.showAll(); break;
                 case "0": System.exit(0);
                 default: System.out.println("Sai chuc nang!");
             }
