089a237057cdd281d05c3f8aafcd2f017feb0594
Truong_thinh <letruongthinh006@gmail.com>
2025-11-23 15:22:36 +0700

    Tao model Payment quan ly hoc phi

---
+++ src/Payment.java
@@ -0,0 +1,19 @@
+import java.io.Serializable;
+
+public class Payment implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private String studentId;
+    private double amount;
+    private String date;
+
+    public Payment(String studentId, double amount, String date) {
+        this.studentId = studentId;
+        this.amount = amount;
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "Hoa don: HV " + studentId + " - " + amount + " VND - Ngay: " + date;
+    }
+}
